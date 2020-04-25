398f4c520f663f85cb0b915154750dad6150b6ed
Rosa Carranza <rcarranza1024@yahoo.com>
2020-04-25 20:15:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,24 @@
-class Dog
-  def initialize(dog_name, dog_breed)
-    @name = dog_name
-    @breed = dog_breed
-  end
+ class Dog 
 
-  def name=(dog_name)
-    @name = dog_name
-  end
+    def initialize(name, breed="Mut")
+     @name = name
+     @breed = breed
+   end
 
-  def name
-    @name
-  end
+    def name=(name)
+     @name = name
+   end
 
-  def breed=(breed)
-    @breed = breed
-  end
+    def name
+     @name
+   end
 
-  def breed
-    @breed
-  end 
-end
+    def breed=(breed)
+     @breed = breed
+   end
+
+    def breed
+     @breed
+   end
+
+  end
