e0a88d241c59e8affe6fa0eee590904a015232b1
Rosa Carranza <rcarranza1024@yahoo.com>
2020-04-25 20:09:32 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,13 @@
-class Person 
+class Person
   def initialize(person_name)
     @name = person_name
   end
- 
+
   def name=(person_name)
     @name = person_name
   end
- 
-  def name 
+
+  def name
     @name 
   end
 end
